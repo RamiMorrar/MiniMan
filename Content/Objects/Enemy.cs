f0201347782ca0e266e440b8a5ec6434af2143d9
Rami Morrar <ramimorrar97@gmail.com>
2021-06-21 11:07:37 -0700

    Enemy Change

--- Content/Objects/Enemy.cs
+++ Content/Objects/Enemy.cs
@@ -3,71 +3,85 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Timers;
 
 namespace Chapter6Game.Content.Objects
 {
-    public class Enemy 
+    public class Enemy
     {
         // Used to handle enemies currently active
         //public List<Enemy> enemies = new List<Enemy>();
 
-      
-       public bool enemyFlip;
-     public   float health;
+
+        public bool enemyFlip;
+        public float speed;
         bool isdead;
-       // public Player playerFunctions = new Player();
-     public Rectangle enemyRect;
+        // public Player playerFunctions = new Player();
+        public Rectangle enemyRect;
         public Enemy()
         {
             isdead = false;
-            
+
 
         }
 
         public static List<Enemy> enemies = new List<Enemy>();
 
+        public Enemy(Rectangle rectangle, float Speed)
+        {
+            this.enemyRect = rectangle;
+            this.speed = Speed;
+        }
+
+        
 
         public void Update (GameTime gameTime)
         {
-            if (health <= 0)
-            {
-                foreach(Enemy e in enemies)
-                enemies.Remove(e);
-            }
+            
         }
     }
 
     public class RedEnemy : Enemy
     {
 
-        float Health;
         
-   public Rectangle SideHorns, topofHead;
-       
-
-       
+        public Vector2 position;
+        
+      public Rectangle topofHead;
+     
+     public void Initialize()
+        {
+            enemyRect = new Rectangle((int)position.X, (int)position.Y, 48, 13);
+        }
+       public void Update()
+        {
+            
+        }
     }
 
     public class BlueEnemy: Enemy
     {
         
         public Vector2 Position;
-        public Rectangle headHorn, Body;
+        public Rectangle headHorn;
         
 
       
         public void Update(GameTime gameTime)
         {
-           // float distance = MathHelper.Distance(playerFunctions.position.X, Position.X);
-            //if (distance < 20)
-            //{
-            //    //add patrol logic
-            //}
+           
         }
     }
     public class SamuraiBoss: Enemy
     {
-        
+        public Vector2 Position;
+
+        public Rectangle SwordAttack;
+        Timer timer;
+        public void OnHit()
+        {
+
+        }
     }
 
 
